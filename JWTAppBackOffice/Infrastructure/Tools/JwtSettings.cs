7a4d63fddc9acb9c6ef29678c21da50d55ac235a
nazkaptan <naz.kaptan99@gmail.com>
2023-07-15 20:01:34 +0300

    ,

--- JWTAppBackOffice/Infrastructure/Tools/JwtSettings.cs
+++ JWTAppBackOffice/Infrastructure/Tools/JwtSettings.cs
@@ -7,12 +7,12 @@
         ValidIssuer = "http://localhost",
         ClockSkew = TimeSpan.Zero,
         ValidateLifetime = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("barkinbayzin.net6")), //16 karakterden fazla bir şey vermemi bekler !!
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("nazkaptan.net6")), //16 karakterden fazla bir şey vermemi bekler !!
         ValidateIssuerSigningKey = true
          */
         public const string Issuer = "http://localhost";
         public const string Audience = "http://localhost";
-        public const string Key = "barkinbayzin.net6"; //16 karakterden fazla bir şey vermemi bekler !!
+        public const string Key = "nazkaptan.net6"; //16 karakterden fazla bir şey vermemi bekler !!
         public const int Expire = 30;
     }
 }
